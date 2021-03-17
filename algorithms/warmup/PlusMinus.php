b76f9ca0521601d62cfa4a9e032954750941fbc0
Julia.Zorina <julia.zorinaa@gmail.com>
2021-03-17 15:06:22 +0200

    added answers

--- algorithms/warmup/PlusMinus.php
+++ algorithms/warmup/PlusMinus.php
@@ -2,27 +2,79 @@
 
 declare(strict_types=1);
 
-use phpDocumentor\Reflection\Types\Float_;
 
 echo "hello world";
 
 $arr = array(-4, 3, -9, 0, 4, 1);
-function PlusMinus(array $arr): float
+function PlusMinus(array $arr): int
 {
+    // $pos = 0;
+    // $zero = 0;
+    // $neg = 0;
+
     $count = count($arr);
-    $array_new = array();
-    for ($i = 0; $i < $count; $i++) {
-        //echo $arr[$i];
-        if ($arr[$i] > 0) {
-            $arr[$i] /= $count;
-            //echo $arr[$i];
-            $array_new[] = $arr[$i];
-            //echo $array_new;
-            foreach ($array_new as $each_number) {
-                $each_number = (float)$each_number;
-                return (float)$each_number;
-            }
-        }
-    }
+
+    $pos = count(array_filter($arr, function ($i) {
+        return $i > 0;
+    }));
+    //echo $pos;
+
+    $neg = count(array_filter($arr, function ($i) {
+        return $i < 0;
+    }));
+    //echo $neg;
+
+    $zero = count(array_filter($arr, function ($i) {
+        return $i == 0;
+    }));
+    //echo $zero;
+
+    $posformat = floatval($pos / $count);
+    //echo  $posformat;
+    $negformat = floatval($neg / $count);
+    $zeroformat =  floatval($zero / $count);
+
+    $p = number_format($posformat, 6, '.', '');
+    echo $p;
+    $n = number_format($negformat, 6, '.', '');
+    //echo $n;
+    $z = number_format($zeroformat, 6, '.', '');
+    //echo $z;
+
+
+    return (int)$p;
+
+    $stdin = fopen("php://stdin", "r");
+
+    fscanf($stdin, "%d\n", $n);
+
+    fscanf($stdin, "%[^\n]", $arr_temp);
+
+    $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+    plusMinus($arr);
+
+    fclose($stdin);
 }
 echo PlusMinus($arr);
+// $stdin = fopen("php://stdin", "r");
+
+// fscanf($stdin, "%d\n", $n);
+
+// fscanf($stdin, "%[^\n]", $arr_temp);
+
+// $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+// plusMinus($arr);
+
+// fclose($stdin);
+
+// echo PlusMinus($arr);
+
+
+//echo $arr[$i];
+// if ($arr[$i] > 0) {
+//     $arr[$i] /= $count;
+//echo $arr[$i];
+// $array_new[] = $arr[$i];
+//echo $array_new;
