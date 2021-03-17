739d477ff30c21e0203dbd051c963826a324e666
Julia.Zorina <julia.zorinaa@gmail.com>
2021-03-17 15:45:34 +0200

    added last answers

--- algorithms/warmup/PlusMinus.php
+++ algorithms/warmup/PlusMinus.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 echo "hello world";
 
 $arr = array(-4, 3, -9, 0, 4, 1);
-function PlusMinus(array $arr): int
+function PlusMinus(array $arr)
 {
     // $pos = 0;
     // $zero = 0;
@@ -35,14 +35,14 @@ function PlusMinus(array $arr): int
     $zeroformat =  floatval($zero / $count);
 
     $p = number_format($posformat, 6, '.', '');
-    echo $p;
+    //echo $p;
     $n = number_format($negformat, 6, '.', '');
     //echo $n;
     $z = number_format($zeroformat, 6, '.', '');
     //echo $z;
 
+    return ($p . "\n" . $n . "\n" . $z);
 
-    return (int)$p;
 
     $stdin = fopen("php://stdin", "r");
 
@@ -57,24 +57,3 @@ function PlusMinus(array $arr): int
     fclose($stdin);
 }
 echo PlusMinus($arr);
-// $stdin = fopen("php://stdin", "r");
-
-// fscanf($stdin, "%d\n", $n);
-
-// fscanf($stdin, "%[^\n]", $arr_temp);
-
-// $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-// plusMinus($arr);
-
-// fclose($stdin);
-
-// echo PlusMinus($arr);
-
-
-//echo $arr[$i];
-// if ($arr[$i] > 0) {
-//     $arr[$i] /= $count;
-//echo $arr[$i];
-// $array_new[] = $arr[$i];
-//echo $array_new;
