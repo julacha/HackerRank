27e8416ea65ba401069870be1210c583065882fe
Julia.Zorina <julia.zorinaa@gmail.com>
2021-04-02 12:25:02 +0300

    added answer

--- algorithms/warmup/MiniMaxSum.php
+++ algorithms/warmup/MiniMaxSum.php
@@ -5,32 +5,30 @@ declare(strict_types=1);
 echo "hello world";
 
 $arr = array(1, 2, 3, 4, 5);
-// echo $count;
-// echo $sum; 
-function MiniMaxSum($arr): array
+
+function MiniMaxSum($arr): int
 {
     $arr = array(1, 2, 3, 4, 5);
-    //echo $count;
+    $count = count($arr);
     $min_sum = 0;
     $max_sum = 0;
     $min = min($arr);
     $max = max($arr);
     //echo $min;
     //echo $max;
-    $max_sum = array_sum($arr) - $min;
-    //echo $max_sum;
+
     $min_sum = array_sum($arr) - $max;
-    //echo $min_sum;
+    $max_sum = array_sum($arr) - $min;
 
-    return [$min_sum, $max_sum];
-    $stdin = fopen("php://stdin", "r");
-
-    fscanf($stdin, "%[^\n]", $arr_temp);
-
-    $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-    miniMaxSum($arr);
-
-    fclose($stdin);
+    echo $min_sum . ' ' . $max_sum;
 }
 echo MiniMaxSum($arr);
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $arr_temp);
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+miniMaxSum($arr);
+
+fclose($stdin);
